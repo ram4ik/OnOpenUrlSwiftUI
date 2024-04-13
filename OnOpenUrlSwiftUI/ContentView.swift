7f85d9f08748132b733bd641d91511d0191c958f
ram4ik <ram4ik@hotmail.com>
2024-04-13 12:23:17 +0300

    Initial Commit

---
+++ OnOpenUrlSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OnOpenUrlSwiftUI
+//
+//  Created by Ramill Ibragimov on 13.04.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OnOpenUrlSwiftUI/OnOpenUrlSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnOpenUrlSwiftUIApp.swift
+//  OnOpenUrlSwiftUI
+//
+//  Created by Ramill Ibragimov on 13.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct OnOpenUrlSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
